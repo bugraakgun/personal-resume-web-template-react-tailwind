317b8f3b416d4089dcde3afa1ae649bd79cbdf06
Muhammed Buğra Akgün <bugraakgun8@gmail.com>
2023-02-21 00:55:18 +0300

    add My Skills section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import {BsMailbox , BsLinkedin , BsGithub, BsTwitter} from 'react-icons/bs'
+import { DiHtml5, DiJavascript1, DiReact, DiCss3} from 'react-icons/di'
 function Profile() {
   return (
     <>
@@ -35,23 +36,54 @@ function Profile() {
         </div>
       </div>
       <div className='h-[36rem] bg-white flex flex-col justify-center items-center px-96'>
-          <h1 className='text-4xl font-black tracking-widest'>ABOUT ME</h1>
-          <h2 className='text-xl font-medium tracking-wider mt-9'>Junior Front End Developer</h2>
-          <p className='text-lg font-light tracking-wider mt-9'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
-          electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of 
-          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <div className=' mt-16'>
-            <button className=' px-16 py-5 border-theme_red border  transition-all hover:opacity-80'>
-              <a href="#" className='text-theme_red text-lg  font-medium '>Hire Me</a>
-            </button>
-            <button className=' px-16 py-5 border-theme_red border ml-10 transition-all hover:opacity-80'>
-              <a href="#" className='text-theme_red text-lg  font-medium'>Download Resume</a>
-            </button>
+        <h1 className='text-4xl font-black tracking-widest'>ABOUT ME</h1>
+        <h2 className='text-xl font-medium tracking-wider mt-9'>Junior Front End Developer</h2>
+        <p className='text-lg font-light tracking-wider mt-9'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
+        electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of 
+        Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <div className=' mt-16'>
+          <button className=' px-16 py-5 border-theme_red border  transition-all hover:opacity-80'>
+            <a href="#" className='text-theme_red text-lg  font-medium '>Hire Me</a>
+          </button>
+          <button className=' px-16 py-5 border-theme_red border ml-10 transition-all hover:opacity-80'>
+            <a href="#" className='text-theme_red text-lg  font-medium'>Download Resume</a>
+          </button>
+        </div>
+      </div>
+      <div className='h-[38rem] bg-table bg-cover  bg-blend-darken bg-dark_effect flex flex-col justify-center items-center'>
+          <span className='text-white text-4xl font-black tracking-widest'>MY SKILLS</span>
+          <span className='text-white text-2xl font-light tracking-widest mt-10'>Web Developer</span>
+          <div className='flex flex-row text-center mt-12 gap-10'>
+              <div className='h-60 w-60 rounded-full border-4 border-theme_red bg-white flex justify-center items-center flex-col gap-y-3 hover:border-8 transition-all'>
+                <span href="#" className='text-4xl bg-theme_red inline-block rounded-full p-3 hover:opacity-80 transition-all hover:text-blue-600'>
+                  <DiJavascript1 className='text-white'/>
+                </span>
+                <h3 className='text-black font-black text-2xl -tracking-wide'>JavaScript</h3>
+              </div>
+              <div className='h-60 w-60 rounded-full border-4 border-theme_red bg-white flex justify-center items-center flex-col gap-y-3 hover:border-8 transition-all'>
+              <span href="#" className='text-4xl bg-theme_red inline-block rounded-full p-3 hover:opacity-80 transition-all hover:text-blue-600'>
+                  <DiReact className='text-white'/>
+                </span>
+                <h3 className='text-black font-black text-2xl -tracking-wide'>ReactJS</h3>
+              </div>
+              <div className='h-60 w-60 rounded-full border-4 border-theme_red bg-white flex justify-center items-center flex-col gap-y-3 hover:border-8 transition-all'>
+              <span href="#" className='text-4xl bg-theme_red inline-block rounded-full p-3 hover:opacity-80 transition-all hover:text-blue-600'>
+                  <DiHtml5 className='text-white'/>
+                </span>
+                <h3 className='text-black font-black text-2xl -tracking-wide'>HTML5</h3>
+              </div>
+              <div className='h-60 w-60 rounded-full border-4 border-theme_red bg-white flex justify-center items-center flex-col gap-y-3 hover:border-8 transition-all'>
+              <span href="#" className='text-4xl bg-theme_red inline-block rounded-full p-3 hover:opacity-80 transition-all hover:text-blue-600'>
+                  <DiCss3 className='text-white'/>
+                </span>
+                <h3 className='text-black font-black text-2xl -tracking-wide'>CSS3</h3>
+              </div>
           </div>
       </div>
+      
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,15 @@ module.exports = {
         footer:'#0e0e0e',
         main_icons:'#e65550',
         theme_red:'#e65550',
+        dark_effect:'#000000b3',
       },
       backgroundImage:{
         'profile':"url('img/profile.jpg')",
         'blog':"url('img/blog.jpg')",
         'portfolio':"url('img/portfolio.jpg')",
         'contact':"url('img/contact.jpg')",
-        'about-section':"url('img/about-section.jpg')"
+        'about-section':"url('img/about-section.jpg')",
+        'table':"url('img/table.jpg')",
       },
       boxShadow:{
         'main':'0 0 70px 2px rgba(0,0,0,0.91) inset',
