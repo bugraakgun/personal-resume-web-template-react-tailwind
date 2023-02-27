91e4eb20ee4161d42ef1449a66f9e00f806d1fde
Muhammed Buğra Akgün <bugraakgun8@gmail.com>
2023-02-27 22:17:52 +0300

    add slider for what the say section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import pp1 from  '../img/profile-pp-1.png'
+import pp1 from '../img/profile-pp-1.png'
 import { BsMailbox, BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs'
 import { DiHtml5, DiJavascript1, DiReact, DiCss3 } from 'react-icons/di'
-
+import Slider from "react-slick";
 function Profile() {
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  };
   return (
     <>
       <Header />
@@ -139,10 +149,12 @@ function Profile() {
             Web & Embedded Aplication
           </span>
         </div>
-        <div className='grid justify-center content-center grid-cols-2 w-2/3 gap-x-9'>
-          <div className='bg-white p-14'>
+        <Slider {...settings} className="w-2/3 ">
+
+
+          <div className='bg-white p-14 '>
             <h2 className='text-black font-black text-xl tracking-widest'>
-              WHAT THEY SAY
+              WHAT THEY SAY -1
             </h2>
             <span className='text-xl font-normal text-theme_red tracking-wide block mt-3'>
               Facebook Inc.
@@ -151,13 +163,13 @@ function Profile() {
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
               dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
-            <img src={pp1} alt="profile-1" className='w-28 h-28 absolute '/>
+            <img src={pp1} alt="profile-1" className='w-28 h-28 absolute ' />
           </div>
 
 
-          <div className='bg-white p-14'>
+          <div className='bg-white p-14 '>
             <h2 className='text-black font-black text-xl tracking-widest'>
-              WHAT THEY SAY
+              WHAT THEY SAY -2
             </h2>
             <span className='text-xl font-normal text-theme_red tracking-wide block mt-3'>
               Facebook Inc.
@@ -166,9 +178,39 @@ function Profile() {
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
               dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
-            <img src={pp1} alt="profile-1" className='w-28 h-28 absolute '/>
+            <img src={pp1} alt="profile-1" className='w-28 h-28 absolute ' />
           </div>
-        </div>
+
+
+          <div className='bg-white p-14 '>
+            <h2 className='text-black font-black text-xl tracking-widest'>
+              WHAT THEY SAY -3
+            </h2>
+            <span className='text-xl font-normal text-theme_red tracking-wide block mt-3'>
+              Facebook Inc.
+            </span>
+            <p className=' font-light text-gray-800 my-4'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <img src={pp1} alt="profile-1" className='w-28 h-28 absolute ' />
+          </div>
+
+
+          <div className='bg-white p-14 '>
+            <h2 className='text-black font-black text-xl tracking-widest'>
+              WHAT THEY SAY -4
+            </h2>
+            <span className='text-xl font-normal text-theme_red tracking-wide block mt-3'>
+              Facebook Inc.
+            </span>
+            <p className=' font-light text-gray-800 my-4'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+            <img src={pp1} alt="profile-1" className='w-28 h-28 absolute ' />
+          </div>
+        </Slider>
       </div>
       <Footer />
 
