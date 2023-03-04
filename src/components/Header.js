567db43b9ec55a92244034c72b4350aeda85d79f
Muhammed Buğra Akgün <bugraakgun8@gmail.com>
2023-03-05 02:47:20 +0300

    Header section design changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,76 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { BsMailbox, BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs'
+import { NavLink } from "react-router-dom";
+import { BsMailbox, BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
 function Header() {
-  const title = "<BugraAkgun.com.tr />"
+  const logo = "<BugraAkgun.com.tr />";
   return (
     <>
-      <nav className='h-24 grid grid-cols-6  text-center '>
-        <div className='col-span-2  bg-main_white hover:text-3xl transition-all'>
-          <Link className='w-full h-full inline-block  text-2xl font-medium' to="/">
-            <span className='relative top-6  '>{title}</span>
-          </Link>
-        </div>
-        <div className=' bg-gray-700 text-white hover:text-3xl transition-all'>
-          <Link className='w-full h-full inline-block   text-2xl font-medium ' to="/profile">
-            <span className='relative top-6 uppercase '>Profile </span>
-          </Link>
-        </div>
-        <div className=' bg-slate-900 text-white hover:text-3xl transition-all'>
-          <Link className='w-full h-full inline-block text-2xl font-medium' to="/blog">
-            <span className='relative top-6 uppercase '>Blog </span>
-          </Link>
-        </div>
-        <div className=' bg-gray-700 text-white hover:text-3xl transition-all'>
-          <Link className='w-full h-full inline-block text-2xl font-medium' to="/portfolio">
-            <span className='relative top-6 uppercase '>Portfolio </span>
-          </Link>
-        </div>
-        <div className='bg-slate-900 text-white hover:text-3xl transition-all'>
-          <Link className='w-full h-full inline-block text-2xl font-medium' to="/contact">
-            <span className='relative top-6 uppercase '>Contact </span>
-          </Link>
-        </div>
-      </nav>
-      <div className='h-[40rem] bg-about-section bg-cover '>
-        <div className='grid grid-cols-2'>
-          <div className='col-start-2 pt-40'>
-            <span className='text-3xl font-normal text-white tracking-wide block'>
+      <div className="h-[40rem] bg-about-section bg-cover ">
+        <nav className="h-24 flex justify-between items-center text-center px-20">
+          <div>
+            <NavLink
+              className="text-2xl font-medium hover:opacity-80 transition-all text-theme_red"
+              to="/"
+            >
+              {logo}
+            </NavLink>
+          </div>
+          <div className=" grid gap-x-6 grid-flow-col text-white">
+            <NavLink
+              className="hover:opacity-80 transition-all capitalize text-2xl font-medium"
+              to="/profile"
+            >
+              Profile
+            </NavLink>
+
+            <NavLink
+              className=" hover:border-b-2 hover:opacity-80 transition-all capitalize text-2xl font-medium"
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+
+            <NavLink
+              className="hover:border-b-2 hover:opacity-80 transition-all capitalize text-2xl font-medium"
+              to="/portfolio"
+            >
+              Portfolio
+            </NavLink>
+          </div>
+        </nav>
+        <div className="grid grid-cols-2">
+          <div className="col-start-2 pt-40">
+            <span className="text-3xl font-normal text-white tracking-wide block">
               Hi, I' Am
             </span>
-            <span className='text-4xl font-black text-white tracking-widest block mt-3'>
+            <span className="text-4xl font-black text-white tracking-widest block mt-3">
               MUHAMMED BUGRA AKGÜN
             </span>
-            <span className='text-xl font-normal text-theme_red tracking-wide block mt-3'>
+            <span className="text-xl font-normal text-theme_red tracking-wide block mt-3">
               Junior Front End Developer
             </span>
-            <div className='mt-12'>
-              <a href="#" className='text-3xl bg-main_white inline-block rounded-full p-3 hover:opacity-80 transition-all hover:text-theme_red'>
+            <div className="mt-12">
+              <a
+                href="mailto:bugraakgun8@gmail.com"
+                className="text-3xl bg-main_white inline-block rounded-full p-3 hover:opacity-80 transition-all hover:text-theme_red"
+              >
                 <BsMailbox />
               </a>
-              <a href="#" className='text-3xl bg-main_white inline-block rounded-full p-3 ml-4 hover:opacity-80 transition-all hover:text-theme_red'>
+              <a
+                href="https://linkedin.com/in/mbugra"
+                className="text-3xl bg-main_white inline-block rounded-full p-3 ml-4 hover:opacity-80 transition-all hover:text-theme_red"
+              >
                 <BsLinkedin />
               </a>
-              <a href="#" className='text-3xl bg-main_white inline-block rounded-full p-3 ml-4 hover:opacity-80 transition-all hover:text-theme_red'>
+              <a
+                href="https://github.com/bugraakgun"
+                className="text-3xl bg-main_white inline-block rounded-full p-3 ml-4 hover:opacity-80 transition-all hover:text-theme_red"
+              >
                 <BsGithub />
               </a>
-              <a href="#" className='text-3xl bg-main_white inline-block rounded-full p-3 ml-4 hover:opacity-80 transition-all hover:text-theme_red'>
+              <a
+                href="#"
+                className="text-3xl bg-main_white inline-block rounded-full p-3 ml-4 hover:opacity-80 transition-all hover:text-theme_red"
+              >
                 <BsTwitter />
               </a>
             </div>
@@ -62,7 +78,7 @@ function Header() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
